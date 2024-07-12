9af82545a05bcc4131968d7231522a3390cf2aba
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-12 23:07:04 +0400

    Update _app.js

--- store-app/pages/_app.js
+++ store-app/pages/_app.js
@@ -1,6 +1,32 @@
 import '../styles/globals.css'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { AwsRum } from 'aws-rum-web';
+import config from '../../aws-backend-config.json';
+
+try {
+  const rumConfig = {
+    sessionSampleRate: 1,
+    identityPoolId: config.rumMonitorIdentityPoolId,
+    endpoint: `https://dataplane.rum.${config.aws_region}.amazonaws.com`,
+    telemetries: ["errors","performance","http"],
+    allowCookies: true,
+    enableXRay: false
+  };
+
+  const APPLICATION_ID = config.rumMonitorId;
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = config.aws_region;
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    rumConfig
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
