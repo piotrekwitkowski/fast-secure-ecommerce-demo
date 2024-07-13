3cf0e89aa92a053667d35ae58ea6f629ae6e9e0b
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-13 22:59:11 +0400

    Update store-infra-stack.ts

--- store-infra/lib/store-infra-stack.ts
+++ store-infra/lib/store-infra-stack.ts
@@ -262,7 +262,7 @@ export class StoreInfraStack extends cdk.Stack {
       '/aws/service/canonical/ubuntu/server/focal/stable/current/amd64/hvm/ebs-gp2/ami-id',
       { os: ec2.OperatingSystemType.LINUX }
     );
-    const instance = new ec2.Instance(this, 'store_backend_ec', {
+    const instance = new ec2.Instance(this, 'store_backend_ec2', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
       machineImage: ubuntu,
       vpc,
