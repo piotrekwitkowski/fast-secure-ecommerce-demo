d8e064c239ddad981f460d9c8ac3417e835fd98f
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-18 10:36:58 +0400

    Update next.config.mjs

--- store-app/next.config.mjs
+++ store-app/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     loader: 'custom',
     loaderFile: './ImageCustomLoader.js',
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [320, 640, 1080, 2048],
   },
 };
 
