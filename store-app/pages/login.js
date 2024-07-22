eac053a064c0dbc66503d1f63776225f2c162ba6
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-22 19:49:11 +0400

    Update login.js

--- store-app/pages/login.js
+++ store-app/pages/login.js
@@ -1,106 +1,43 @@
-import Layout from './components/Layout';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { login } from '../utils/auth';
+import { sign } from 'jsonwebtoken';
 
-export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { unmarshall } from '@aws-sdk/util-dynamodb'
+import config from '../../aws-backend-config.json';
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+// Initialize the DynamoDB client
+const client = new DynamoDBClient({
+  region: config.aws_region,
+});
 
-    function setError(err) {
-      document.getElementById("error").innerHTML = err;
-    }
+const docClient = DynamoDBDocumentClient.from(client);
 
-    // Basic form validation
-    if (!username || !password) {
-      setError('Please enter both username and password');
-      return;
-    }
+const SECRET_KEY =  config.login_secret_key; // In a real app, use an environment variable
 
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
-      });
-      
-      if (response.ok) {
-        // Login successful
-        const data = await response.json();
-        login(username, data.token)
-        router.push('/'); // Redirect to home page
-      } else {
-        // Login failed
-        if ((response.status === 405) || (response.status === 202)) {
-          // reload the page to display the captcha or challenge
-          router.reload()
-        } else if (response.status === 403) {
-          setError('Login was not authorized and blocked');
-        } else {
-          const data = await response.json();
-          setError(data.message || 'Login failed');
-        }
-        
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  const { username, password } = req.body;
+
+  const command = new GetItemCommand({
+    'TableName': config.users_ddb_table,
+    'Key': {
+      'username' : {
+        'S': username
       }
-    } catch (err) {
-      setError('An error occurred. Please try again.');
-      console.log('Login error:', err);
     }
-  };
+  });
 
-  return (
-    <Layout>
-      <div className="max-w-md mx-auto">
-        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="******************"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign In
-            </button>
-          </div>
-          <div className="flex items-center justify-between">
-              <Link href={`/register`} className="py-2 px-2 font-small text-blue-500">Sign up here, if you do not have an account</Link>
-          </div>
+  const response = await docClient.send(command);
 
-        </form>
-        <div id='error' className="flex items-center justify-between text-red-500"/>
-      </div>
-    </Layout>
-  );
+  // In a real application, you would check these credentials against your database
+  if (response.Item && unmarshall(response.Item).password === password) {
+    const premium = unmarshall(response.Item).premium;
+    const token = sign({ username, premium }, SECRET_KEY, { expiresIn: '2h' });
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
 }
