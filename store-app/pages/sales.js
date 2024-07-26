4d17cb747419ec8c3cd2eea0aeb278e2e6b2f21f
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-26 09:38:11 +0400

    Update sales.js

--- store-app/pages/sales.js
+++ store-app/pages/sales.js
@@ -1,4 +1,3 @@
-
 import Layout from './components/Layout';
 
 export default function Sales({ }) {
@@ -6,12 +5,15 @@ export default function Sales({ }) {
         <Layout>
             <div className="container mx-auto p-4 bg-gray-100 min-h-screen">
                 <main className="flex flex-col items-center space-y-6">
-                    <h1 className="text-3xl font-bold text-green-600 mb-4">50% discount this week</h1>
+                    <h1 className="text-3xl font-bold text-green-600">50% discount this week</h1>
+                    <h3 className="text-l text-green-600">Use GOGREEN discount code!</h3>
                     <div className="rounded-lg overflow-hidden shadow-lg">
                         <video
                             autoPlay
                             loop
                             playsInline
+                            muted
+                            controls
                             className="max-w-full"
                         >
                             <source src="sales.mp4" type="video/mp4" />
