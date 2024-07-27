82714fe88a9d27648038e551d1a5d6a9be9e711f
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-27 15:12:26 +0400

    Update index-v2.js

--- store-app/pages/index-v2.js
+++ store-app/pages/index-v2.js
@@ -2,37 +2,55 @@ import Layout from './components/Layout';
 import Link from 'next/link';
 import Image from 'next/image';
 import Script from 'next/script';
+import { addItem } from '../utils/cart';
+import { useRouter } from 'next/router';
+import { isLoggedIn } from '../utils/auth';
+import { useState, useEffect } from 'react';
 
 export default function Home({ products }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsAuthenticated(isLoggedIn());
+  }, []);
+
+  function addItemToCart(id, price) {
+    addItem(id, price);
+    router.reload();
+  }
+
   return (
     <Layout>
       <Script type="speculationrules" id="speculationAPI" dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "prerender": [
-              {
-                "source": "document",
-                "where": { "href_matches": "/product/*" },
-                "eagerness": "moderate"
-              }
-            ]
-          })
-        }}>
+        __html: JSON.stringify({
+          "prerender": [
+            {
+              "source": "document",
+              "where": { "href_matches": "/product/*" },
+              "eagerness": "moderate"
+            }
+          ]
+        })
+      }}>
       </Script>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
-          <Link href={`/product/${product.id}`} key={product.id}>
-            <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300">
-            <Image src={product.image} alt={product.name} className="w-full h-400 object-cover" width={200} height={200} loading="lazy"/>
-              <div className="p-4">
-                <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-                <p className="text-gray-600 mb-4">{product.description.substring(0, 60)}...</p>
-                <span className="text-green-600 font-bold">${product.price}</span>
-                <button className="float-end bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300">
-                  Add to Cart
-                </button>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <Image src={product.image} alt={product.name} className="w-full h-400 object-cover" width={200} height={200} loading="lazy" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{product.name}</div>
+                <p className="text-gray-700 text-base">{product.description.substring(0, 60)}...</p>
               </div>
+            </Link>
+            <div className="px-6 pt-4 pb-2 flex justify-between items-center">
+              <span className="text-xl font-bold text-green-600">${product.price}</span>
+              <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded inline-flex items-center" onClick={() => { addItemToCart(product) }}>
+                <span>Add to Cart</span>
+              </button>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     </Layout>
