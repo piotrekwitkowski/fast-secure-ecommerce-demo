431c41aa0950332a58cdd2c08231990a16127092
Achraf Souk <48942933+achrafsouk@users.noreply.github.com>
2024-07-27 14:24:44 +0400

    Update comment.js

--- store-app/pages/api/comment.js
+++ store-app/pages/api/comment.js
@@ -1,5 +1,6 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, PutItemCommand, QueryCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { unmarshall } from '@aws-sdk/util-dynamodb'
 import config from '../../aws-backend-config.json';
 
 // Initialize the DynamoDB client
@@ -11,49 +12,60 @@ const docClient = DynamoDBDocumentClient.from(client);
 
 
 export default async function handler(req, res) {
-  console.log(req.body);
-  if (req.method !== 'POST') {
-    return res.status(403).end(); // Method Not Allowed
-  } else res.status(200).json({ message: 'comment added successfully'});
+  if (req.method === 'GET') {
+    try {
 
-  //const { username, comment, pid } = req.body;
+      const command = new QueryCommand({
+        'TableName': config.comments_ddb_table,
+        'KeyConditionExpression': "productid = :pid",
+        'ExpressionAttributeValues': {
+          ':pid': {
+            "S": req.query.productId
+          }
+        }
+      });
+    
+      const response = await docClient.send(command);
+      console.log(response.Items);
+      const unmarshalled = response.Items.map((i) => unmarshall(i));
 
-  // Basic validation
-  /*if (!username || !password || !phone || !address) {
-    return res.status(400).json({ message: 'All fields are required' });
-  }*/
-
-  /* Check if user already exists
-  if (users.find(user => user.username === username)) {
-    return res.status(409).json({ message: 'Username already exists' });
-  }*/
-
-  /*const command = new PutItemCommand({
-    'TableName': config.users_ddb_table,
-    'Item': {
-      'username' : {
-        'S': username
-      }, 
-      'password' : {
-        'S': password
-      }, 
-      'phone' : {
-        'S': phone
-      }, 
-      'address' : {
-        'S': address
-      }, 
+      res.status(200).json(unmarshalled);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'comments couldn\'t be retrieved' });
     }
-  });*
+  } else if (req.method === 'POST') {
+    const { username, text, productid, timestamp } = req.body;
+    if (!username || !text || !productid || !timestamp) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
 
-  try {
-    await docClient.send(command);
-    res.status(200).json({ message: 'User registered successfully'});
+    const command = new PutItemCommand({
+      'TableName': config.comments_ddb_table,
+      'Item': {
+        'username': {
+          'S': username
+        },
+        'productid': {
+          'S': productid
+        },
+        'text': {
+          'S': text
+        },
+        'timestamp': {
+          'N': `${timestamp}`
+        },
+      }
+    });
+    try {
+      await docClient.send(command);
+      res.status(200).json({ message: 'comment added successfully' });
 
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: 'User registeration failed'});
-  } */
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'comment couldn\'t be added' });
+    }
+  } return res.status(400).json({ message: 'Bad request' });
+
 
 }
-  
